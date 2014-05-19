dd832978c65c7b7d00ebf35af03558996992420c
Ariel Nunez <ingenieroariel@gmail.com>
2014-05-19 10:20:35 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 
 setup(
-    name = "user-messages",
+    name = "geonode-user-messages",
     version = "0.1",
     author = "Eldarion",
     author_email = "development@eldarion.com",
-    description = "a reusable private user messages application for Django",
+    description = "Fork of user-messages: a reusable private user messages application for Django",
     long_description = open("README.rst").read(),
     license = "BSD",
-    url = "http://github.com/eldarion/user_messages",
+    url = "http://github.com/GeoNode/user_messages",
     packages = [
         "user_messages",
         "user_messages.tests",
