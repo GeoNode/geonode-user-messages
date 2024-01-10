b28de2a2d0f6d6708462e7b2e15e3fe206a76889
mattiagiupponi <mattia.giupponi@gmail.com>
2024-01-10 15:57:58 +0100

    Upgrade Django version

--- user_messages/urls.py
+++ user_messages/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url
-
+from django.urls import re_path
 from . import views
 
 urlpatterns = [  # "user_messages.views",
-    url(r"^inbox/$", views.inbox, name="messages_inbox"),
-    url(r"^create/$", views.message_create, name="message_create"),
-    url(r"^create/(?P<user_id>\d+)/$", views.message_create, name="message_create"),
-    url(r"^create/_multiple/$", views.message_create, name="message_create_multiple"),
-    url(r"^thread/(?P<thread_id>\d+)/$", views.thread_detail,
+    re_path(r"^inbox/$", views.inbox, name="messages_inbox"),
+    re_path(r"^create/$", views.message_create, name="message_create"),
+    re_path(r"^create/(?P<user_id>\d+)/$", views.message_create, name="message_create"),
+    re_path(r"^create/_multiple/$", views.message_create, name="message_create_multiple"),
+    re_path(r"^thread/(?P<thread_id>\d+)/$", views.thread_detail,
         name="messages_thread_detail"),
-    url(r"^thread/(?P<thread_id>\d+)/delete/$", views.thread_delete,
+    re_path(r"^thread/(?P<thread_id>\d+)/delete/$", views.thread_delete,
         name="messages_thread_delete"),
 ]
